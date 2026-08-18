49a80a4ea9f262779c517f3eb2fe900ba51f31fa
gagagaguguhh-cmd <gagagaguguhh@gmail.com>
2026-08-18 17:32:52 +0700

    update/add src/main/java/com/medieval/economy/MedievalEconomyPlugin.java

--- src/main/java/com/medieval/economy/MedievalEconomyPlugin.java
+++ src/main/java/com/medieval/economy/MedievalEconomyPlugin.java
@@ -3,10 +3,7 @@ package com.medieval.economy;
 import com.medieval.economy.commands.*;
 import com.medieval.economy.listeners.InventoryClickListener;
 import com.medieval.economy.listeners.PlayerJoinListener;
-import com.medieval.economy.managers.AuctionManager;
-import com.medieval.economy.managers.EconomyManager;
-import com.medieval.economy.managers.SellManager;
-import com.medieval.economy.managers.ShopManager;
+import com.medieval.economy.managers.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MedievalEconomyPlugin extends JavaPlugin {
@@ -15,6 +12,7 @@ public class MedievalEconomyPlugin extends JavaPlugin {
     private ShopManager shopManager;
     private SellManager sellManager;
     private AuctionManager auctionManager;
+    private ScoreboardManager scoreboardManager;
 
     @Override
     public void onEnable() {
@@ -23,20 +21,22 @@ public class MedievalEconomyPlugin extends JavaPlugin {
         this.shopManager = new ShopManager();
         this.sellManager = new SellManager();
         this.auctionManager = new AuctionManager(this);
+        this.scoreboardManager = new ScoreboardManager(this, economyManager);
 
         // Register commands
         getCommand("balance").setExecutor(new BalanceCommand(economyManager));
-        getCommand("pay").setExecutor(new PayCommand(economyManager));
+        getCommand("pay").setExecutor(new PayCommand(economyManager, scoreboardManager));
         getCommand("akun").setExecutor(new AkunCommand(economyManager));
         getCommand("shop").setExecutor(new ShopCommand(shopManager, economyManager));
         getCommand("sell").setExecutor(new SellCommand());
         getCommand("auctions").setExecutor(new AuctionsCommand(auctionManager, economyManager));
+        getCommand("settings").setExecutor(new SettingsCommand(scoreboardManager));
 
         // Register event listeners
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(economyManager), this);
-        getServer().getPluginManager().registerEvents(new InventoryClickListener(economyManager, shopManager, sellManager, auctionManager), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(economyManager, scoreboardManager), this);
+        getServer().getPluginManager().registerEvents(new InventoryClickListener(economyManager, shopManager, sellManager, auctionManager, scoreboardManager), this);
 
-        getLogger().info("MedievalEconomy plugin 1.0.0 berhasil diaktifkan!");
+        getLogger().info("MedievalEconomy plugin 1.0.0 (Scoreboard & Settings) berhasil diaktifkan!");
     }
 
     @Override
