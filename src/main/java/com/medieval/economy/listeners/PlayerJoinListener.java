06f76878511cb104080f7762d6d87cbf27c91fe6
gagagaguguhh-cmd <gagagaguguhh@gmail.com>
2026-08-18 17:33:01 +0700

    update/add src/main/java/com/medieval/economy/listeners/PlayerJoinListener.java

--- src/main/java/com/medieval/economy/listeners/PlayerJoinListener.java
+++ src/main/java/com/medieval/economy/listeners/PlayerJoinListener.java
@@ -1,6 +1,7 @@
 package com.medieval.economy.listeners;
 
 import com.medieval.economy.managers.EconomyManager;
+import com.medieval.economy.managers.ScoreboardManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -8,13 +9,16 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerJoinListener implements Listener {
 
     private final EconomyManager economyManager;
+    private final ScoreboardManager scoreboardManager;
 
-    public PlayerJoinListener(EconomyManager economyManager) {
+    public PlayerJoinListener(EconomyManager economyManager, ScoreboardManager scoreboardManager) {
         this.economyManager = economyManager;
+        this.scoreboardManager = scoreboardManager;
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         economyManager.registerPlayer(event.getPlayer().getUniqueId(), event.getPlayer().getName());
+        scoreboardManager.setupScoreboard(event.getPlayer());
     }
 }
